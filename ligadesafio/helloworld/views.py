a5c93d10095df0d7d959ee853347c525eccf7cba
Freitas-gui <freitas-gui19@hotmail.com>
2020-02-15 16:39:52 -0300

    para controle de versoes em um projeto compartilhado remove projeto back-end

--- ligadesafio/helloworld/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def helloworld(request):
-    return render(request, 'helloworld/helloworld.html')
-# Create your views here.
